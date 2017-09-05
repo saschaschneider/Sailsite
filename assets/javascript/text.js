e9e2d3e21a010513d35373849d080c384b266d28
saschaschneider <s4schnei@gmail.com>
2017-09-04 22:47:13 -0700

    V1

---
+++ assets/javascript/text.js
@@ -0,0 +1,48 @@
+//Article #5 ID: 0000006
+document.getElementById("0000006T").innerHTML = "The Stopper Knot"; // Title
+document.getElementById("0000006C").innerHTML = "The Double Overhand Stopper Knot, or short Stopper Knot, helps to create a knot in the end of a line to prevent it from slipping through a whole, i.e. from a rope clutch "; // Content 
+document.getElementById("0000006C").innerHTML +=" <a href='https://www.w3schools.com/html/html_links.asp'>Learn how to do a Stopper Knot now! </a>";
+document.getElementById("0000006IMG").src = "assets/images/stopperknot.jpg"; // Image
+//document.getElementById("0000005S").innerHTML = ""; // Author if anyhting needs to be quoted 
+
+//Article #5 ID: 0000005
+document.getElementById("0000005T").innerHTML = "The Bowline"; // Title
+document.getElementById("0000005C").innerHTML = "A very useful knot in all kinds of situations. Whether you want to connect two lines, or a knot that doesn't run or slip - you found the perfect knot "; // Content 
+document.getElementById("0000005C").innerHTML +=" <a href='https://www.w3schools.com/html/html_links.asp'>Test your Bowline skills now! </a>";
+document.getElementById("0000005IMG").src = "assets/images/bowline.jpg"; // Image
+//document.getElementById("0000005S").innerHTML = ""; // Author if anyhting needs to be quoted 
+
+// Article #4 ID: 0000004
+document.getElementById("0000004C").innerHTML = "When I'm not surfing or sailing, I am to be found at the harbour working on my boat."
+document.getElementById("0000004S").innerHTML = "Laura Dekker"
+// src : https://www.brainyquote.com/quotes/quotes/l/lauradekke680476.html?src=t_sailing
+
+
+// Article #3 ID: 0000003
+document.getElementById("0000003C").innerHTML = "I'd love to have a proper sailing boat and go around the world."
+document.getElementById("0000003S").innerHTML = "Jonathan Powell"
+// src : https://www.brainyquote.com/quotes/keywords/sailing.html";
+
+
+// Article #2 ID: 0000002
+document.getElementById("0000002T").innerHTML = "The Sheet Bend!";
+document.getElementById("0000002C").innerHTML = "Easy to untie, but super secur - tie two lines together in a heartbeat"
+document.getElementById("0000002IMG").src = "assets/images/sheetbend.jpg";
+// img src: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfE3DI6dNBJh_suvH30K61wSEOpKQkQQR9vuyrRWnkxzQZ4JrYaw
+
+
+// Article #1 ID: 0000001
+document.getElementById("0000001T").innerHTML = "The Cleat Hitch!";
+document.getElementById("0000001C").innerHTML = "If there is a cleat and there will be, the Cleat Hitch will be present. And  yet, there are still many sailors who don’t know how to do it right. An improperly secured knot can have many consequences, so take 5mins and learn how to do this easy, but very useful know.";
+document.getElementById("0000001C").innerHTML += " <a href='https://www.w3schools.com/html/html_links.asp'>Read the full article!</a>";
+document.getElementById("0000001IMG").src = "assets/images/cleathitch.jpg";
+// img src: https://cdn.pixabay.com/photo/2017/07/12/19/32/boat-2498064__480.jpg
+
+// Template to add content to the site 
+
+// Article #x ID: 000000X
+// document.getElementById("000000xT").innerHTML = ""; // Title
+// document.getElementById("000000xC").innerHTML = ""; // Content 
+// document.getElementById("000000xC").innerHTML +=""; // Call to action
+// document.getElementById("000000xIMG").src = "assets/images/..."; // Image
+// document.getElementById("000000xS").innerHTML = ""; // Author if anyhting needs to be quoted 
